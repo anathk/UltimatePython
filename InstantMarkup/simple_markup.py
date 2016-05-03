a290e8b42774a6936085f33776c3f2ed06b7d1ab
Zhengchao Tian <zhengchaotian@gmail.com>
2016-05-03 16:55:32 -0400

    Simple makeup.

--- InstantMarkup/simple_markup.py
+++ InstantMarkup/simple_markup.py
@@ -1,2 +1,22 @@
 import sys, re
+from InstantMarkup.util import *
 
+print('<html><head><title>...</title><body>')
+
+title = True
+file = open('test_input.txt')
+for block in blocks(file):
+    block = re.sub(r'\*(.+?)\*', r'<em>\1</em>', block)
+    if title:
+        print('<h1')
+        print(block)
+        print('</h1>')
+        title = False
+    else:
+        print('<p')
+        print(block)
+        print('</p>')
+
+file.close()
+
+print('</body></html>')
