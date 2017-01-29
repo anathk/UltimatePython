5e54e8e48e0c635fbed9723d3c6a2f5363ec8939
Zhengchao Tian <zhengchaotian@gmail.com>
2017-01-29 18:24:27 -0500

    Added starter code and mnist dataset.

---
+++ HW01/scipy/softmax.py
@@ -0,0 +1,60 @@
+import numpy as np
+from scipy.sparse import coo_matrix
+
+def softmaxCost(theta, numClasses, inputSize, decay, data, labels):
+  """Computes and returns the (cost, gradient)
+
+  numClasses - the number of classes 
+  inputSize - the size N of the input vector
+  lambda - weight decay parameter
+  data - the N x M input matrix, where each row data[i, :] corresponds to
+         a single sample
+  labels - an M x 1 matrix containing the labels corresponding for the input data
+  """
+
+  # Unroll the parameters from theta
+  theta = np.reshape(theta, (numClasses, inputSize))
+
+  numCases = data.shape[1]
+
+  groundTruth = coo_matrix((np.ones(numCases, dtype = np.uint8),
+                            (labels, np.arange(numCases)))).toarray()
+  cost = 0;
+  thetagrad = np.zeros((numClasses, inputSize))
+
+  ## ---------- YOUR CODE HERE --------------------------------------
+  #  Instructions: Compute the cost and gradient for softmax regression.
+  #                You need to compute thetagrad and cost.
+  #                The groundTruth matrix might come in handy.
+
+
+
+
+  # ------------------------------------------------------------------
+  # Unroll the gradient matrices into a vector for the optimization function.
+  grad = thetagrad.ravel()
+
+  return cost, grad
+
+
+def softmaxPredict(theta, data):
+  """Computes and returns the softmax predictions in the input data.
+
+  theta - model parameters trained using fmin_l_bfgs_bin softmaxExercise.py,
+          a numClasses x inputSize matrix.
+  data - the M x N input matrix, where each row data[i,:] corresponds to
+         a single sample.
+  """
+
+  #  Your code should produce the prediction matrix pred,
+  #  where pred(i) is argmax_c P(c | x(i)).
+ 
+  ## ---------- YOUR CODE HERE --------------------------------------
+  #  Instructions: Compute pred using theta assuming that the labels start 
+  #                from 0.
+
+
+
+  # ---------------------------------------------------------------------
+
+  return pred
---
+++ HW01/tensorflow/softmax.py
@@ -0,0 +1,110 @@
+"""Builds the MNIST softmax network.
+
+Implements the inference/loss/training pattern for model building.
+
+1. inference() - Builds the model as far as is required for running the network
+forward to make predictions.
+2. loss() - Adds to the inference model the layers required to generate loss.
+3. training() - Adds to the loss model the Ops required to generate and
+apply gradients.
+
+This file is used by the "softmaxExercise.py" files and not meant to be run.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+import tensorflow as tf
+
+# The MNIST dataset has 10 classes, representing the digits 0 through 9.
+NUM_CLASSES = 10
+
+# The MNIST images are always 28x28 pixels.
+IMAGE_SIZE = 28
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+
+
+def inference(images):
+  """Build the MNIST model up to where it may be used for inference.
+
+  Args:
+    images: Images placeholder, from inputs().
+    
+  Returns:
+    logits: Output tensor with the computed logits.
+  """
+
+  with tf.name_scope('softmax_linear'):
+  ## ---------- YOUR CODE HERE --------------------------------------
+
+
+  # ------------------------------------------------------------------
+
+  return logits
+
+
+def loss(logits, labels):
+  """Calculates the loss from the logits and the labels.
+
+  Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size].
+
+  Returns:
+    loss: Loss tensor of type float.
+  """
+  ## ---------- YOUR CODE HERE --------------------------------------
+
+
+  # ------------------------------------------------------------------
+
+
+def training(loss, learning_rate):
+  """Sets up the training Ops.
+
+  Creates a summarizer to track the loss over time in TensorBoard.
+
+  Creates an optimizer and applies the gradients to all trainable variables.
+
+  The Op returned by this function is what must be passed to the
+  `sess.run()` call to cause the model to train.
+
+  Args:
+    loss: Loss tensor, from loss().
+    learning_rate: The learning rate to use for gradient descent.
+
+  Returns:
+    train_op: The Op for training.
+  """
+  # Add a scalar summary for the snapshot loss.
+  tf.summary.scalar('loss', loss)
+  # Create the gradient descent optimizer with the given learning rate.
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+  # Create a variable to track the global step.
+  global_step = tf.Variable(0, name='global_step', trainable=False)
+  # Use the optimizer to apply the gradients that minimize the loss
+  # (and also increment the global step counter) as a single training step.
+  train_op = optimizer.minimize(loss, global_step=global_step)
+
+  return train_op
+
+
+def evaluation(logits, labels):
+  """Evaluate the quality of the logits at predicting the label.
+
+  Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size], with values in the
+      range [0, NUM_CLASSES).
+
+  Returns:
+    A scalar int32 tensor with the number of examples (out of batch_size)
+    that were predicted correctly.
+  """
+
+  ## ---------- YOUR CODE HERE --------------------------------------
+
+
+  # ------------------------------------------------------------------
