b301b72191c179419690bd3290d12f0bb78097eb
anathk <zhengchaotian@gmail.com>
2018-03-12 02:11:27 -0400

    Regular update.

--- MachineLearning/BugReportMapping/BugReportMapping.py
+++ MachineLearning/BugReportMapping/BugReportMapping.py
@@ -14,13 +14,15 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.multiclass import OneVsRestClassifier
 import tensorflow as tf
+from tensorflow.contrib import learn
+import numpy as np
 
 try:
     engine = create_engine('mysql+mysqlconnector://root:@localhost:3306/eclipse_platform_ui', echo=False)
 
     df_ui = pd.read_sql_table('bug_and_files', engine)
 except:
-    df_ui = pd.read_csv("eclipse_platform_ui.csv")
+    df_ui = pd.read_csv("eclipse_platform_ui_no_space.csv")
 
 corpus = []
 labels = []
@@ -47,38 +49,48 @@ for i in range(6495):
 countVectorizer = CountVectorizer()
 
 X_array = countVectorizer.fit_transform(corpus).toarray()
-y_array = countVectorizer.fit_transform(labels).toarray()
+# y_array = countVectorizer.fit_transform(labels).toarray()
+
+max_document_length_y = max([len(x.split("\n")) for x in labels])
+vocab_processor_y = learn.preprocessing.VocabularyProcessor(max_document_length_y)
+y_array = np.array(list(vocab_processor_y.fit_transform(labels)))
 
 X_train_array, X_test_array, y_train_array, y_test_array = train_test_split(X_array, y_array, test_size = 0.2, random_state =0)
 
-pca = PCA(n_components=None)
-X_train_array = pca.fit_transform(X_train_array)
-X_test_array = pca.transform(X_test_array)
-explained_variance = pca.explained_variance_ratio_
+# pca = PCA(n_components=None)
+# X_train_array = pca.fit_transform(X_train_array)
+# X_test_array = pca.transform(X_test_array)
+# explained_variance = pca.explained_variance_ratio_
+print(X_train_array.shape)
 
-#
-# X = tf.placeholder(tf.float32, [None, 11045])
-# W = tf.Variable(tf.zeros([11045, 6122]))
-# b = tf.Variable(tf.zeros([6122]))
-#
-# y = tf.matmul(X, W) + b
-#
-# y_ = tf.placeholder(tf.float32, [None, 6122])
-#
-# cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-#
-# sess = tf.InteractiveSession()
-# tf.global_variables_initializer().run()
-#   # Train
-# for _ in range(2):
-#     #batch_xs, batch_ys = mnist.train.next_batch(100)
-#     sess.run(train_step, feed_dict={X: X_train_array, y_: y_train_array})
-#
-#
-# correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-#
-# print(sess.run(accuracy, feed_dict={X: X_test_array,
-#                                       y_: y_test_array}))
-#
+
+X = tf.placeholder(tf.float32, [5196, 11045])
+W = tf.Variable(tf.zeros([11045, 383]))
+b = tf.Variable(tf.zeros([5196]))
+
+#tf.add(tf.matmul(images, weights1), bias1
+y = tf.add(tf.matmul(X, W), b)
+
+
+
+y_ = tf.placeholder(tf.float32, [5196, 383])
+
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.3)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+tf.global_variables_initializer().run()
+  # Train
+for _ in range(2):
+    #batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={X: X_train_array, y_: y_train_array})
+
+
+correct_prediction = tf.nn.in_top_k(tf.cast(tf.argmax(y, 1), tf.float32), y_, 5)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={X: X_test_array,
+                                      y_: y_test_array}))
+
